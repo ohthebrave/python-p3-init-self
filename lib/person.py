4fa2975255708ffa49a96c6327d45993d0da5a0f
George <georgejoj28@gmail.com>
2023-11-30 12:03:19 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def parent_name(self, parent):
+        self.parent = parent    
+
+
+jon = Person('Joe')
+jon.parent_name('Mose')
+print(jon.parent)
